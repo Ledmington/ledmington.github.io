298bf0f29b39ee33bfd4a36904530ed21f85c5b8
Ledmington <ledmington.dev@gmail.com>
2022-04-13 15:17:21 +0200

    Minor fixes in link_test

--- link_test.py
+++ link_test.py
@@ -4,57 +4,70 @@ import sys
 import requests
 from termcolor import colored
 
-url_regex = r"<a\s+?(?:[\w\-\"\=]+\s+?)?href=\"?((?:https?:\/\/)?(?:[\w\d.:\/]+?))\"?\s*?>"
+url_regex = (
+    r"<a\s+?(?:[\w\-\"\=]+\s+?)?href=\"?((?:https?:\/\/)?(?:[\w\d.:\/]+?))\"?\s*?>"
+)
+
 
 def print_broken_link(link, page, error_code):
-	print("Link", colored('\"'+link+'\"', 'blue'), "in file", colored('\"'+page+'\"', 'blue'), "received code", colored(str(error_code), 'red'))
+    print(
+        "Link",
+        colored('"' + link + '"', "blue"),
+        "in file",
+        colored('"' + page + '"', "blue"),
+        "received code",
+        colored(str(error_code), "red"),
+    )
+
 
 def dfs(file_name, files=[]):
-	if os.path.isdir(file_name):
-		for f in os.listdir(file_name):
-			dfs(file_name+"/"+f)
-	else:
-		if file_name.endswith(".html"):
-			files.append(file_name)
-	return files
+    if os.path.isdir(file_name):
+        for f in os.listdir(file_name):
+            dfs(file_name + "/" + f)
+    else:
+        if file_name.endswith(".html"):
+            files.append(file_name)
+    return files
+
 
 def check_url(url, file):
-	res = requests.get(url)
-	c = res.status_code
+    res = requests.get(url)
+    c = res.status_code
 
-	if not c == 200:
-		print_broken_link(url, file, c)
-		return 1
-	else:
-		return 0
+    if not c == 200:
+        print_broken_link(url, file, c)
+        return 1
+    else:
+        return 0
+
 
 def main():
-	build_dir = "./build"
-	html_files = dfs(build_dir)
-	
-	urls_to_check = []
-	for file in html_files:
-		with open(file) as f:
-			urls = re.findall(url_regex, f.read())
-		for url in urls:
-			urls_to_check.append((url, file))
+    build_dir = "./build"
+    html_files = dfs(build_dir)
 
-	broken_links_found = 0
+    urls_to_check = []
+    for file in html_files:
+        with open(file) as f:
+            urls = re.findall(url_regex, f.read())
+        for url in urls:
+            urls_to_check.append([url, file])
 
-	# add "https://localhost:4000" to each link that starts with "/"
-	for t in urls_to_check:
-		if t[0].startswith("/"):
-			r = check_url("http://localhost:4000"+t[0], t[1])
-		else:
-			r = check_url(t[0], t[1])
-		broken_links_found += r
+    broken_links_found = 0
 
-	if broken_links_found > 0:
-		print("\n", colored(broken_links_found, 'red'), "broken links found")
-		sys.exit(-1)
-	else:
-		print("\n", colored(broken_links_found, 'green'), "broken links found")
-		sys.exit(0)
+    # add "http://localhost:4000" to each link that starts with "/"
+    for t in urls_to_check:
+        if t[0].startswith("/"):
+            t[0] = "http://localhost:4000" + t[0]
+        r = check_url(t[0], t[1])
+        broken_links_found += r
+
+    if broken_links_found > 0:
+        print("\n", colored(broken_links_found, "red"), "broken links found")
+        sys.exit(-1)
+    else:
+        print("\n", colored(broken_links_found, "green"), "broken links found")
+        sys.exit(0)
+
 
 if __name__ == "__main__":
-	main()
+    main()
